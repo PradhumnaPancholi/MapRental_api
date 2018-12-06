0343aea6db2db3d9bbf6c3909c54d720ae90d990
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-12-06 01:13:23 -0500

    setup databse connection

---
+++ MapRental_api/Models/MapRentalModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapRental_api.Models
+{
+    public class MapRentalModel : DbContext
+    {
+        public MapRentalModel(DbContextOptions<MapRentalModel>options) : base(options)
+        {
+
+        }
+
+
+    }
+}
